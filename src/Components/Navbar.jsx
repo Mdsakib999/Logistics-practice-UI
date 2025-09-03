76a3736ff1f0cee0b8f5a999b3107fa39ba34b4f
MuntasirMahmud <muntasirm525@gmail.com>
2025-09-03 15:59:21 +0600

    navbar design started

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import logo from "../assets/Logo.png";
 const Navbar = () => {
   return (
-    <div>
-      <p> Nav bar</p>
+    //header section
+    <div className="bg-[#172a5b]">
+      <div className="header-logo">
+        <img src={logo} className="w-36" alt="logo image" />
+      </div>
+      <div className="header-info">
+        <ul>
+          <li>
+            <div></div>
+            <div>
+              <p></p>
+              <p></p>
+            </div>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Navbar from "../../Components/Navbar";
+
 const Home = () => {
     //   useEffect(() => {
     //     window.scrollTo({ top: 0, behavior: "smooth" });
     //   }, []);
     return (
         <div>
+            <Navbar />
             <p>Home, muntasir vai</p>
         </div>
     );
