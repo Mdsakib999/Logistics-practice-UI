64b23d6aa8e4383af80cfa3c34326ccaec646376
Mdsakib999 <sakibabrar999@gmail.com>
2025-09-03 13:06:11 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+
+const Home = () => {
+      // useEffect(() => {
+      //   window.scrollTo({ top: 0, behavior: "smooth" });
+      // }, []);
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    );
+};
+
+export default Home;
