8712e8326c994b2c8416d6153d7e7879c3b4de4d
imon-n <imon.eeecu@gmail.com>
2025-09-04 14:09:30 +0600

    work on service component

---
+++ src/Components/Ourservice.jsx
@@ -0,0 +1,17 @@
+import Preheading from "../utils/Preheading";
+
+const Ourservice = () => {
+  return (
+    <div>
+      <Preheading>Our Services</Preheading>
+      <h1></h1>
+      <div className="grid grid-cols-4">
+        <div className="bg-[url('https://i.ibb.co.com/XfTb7MXB/Truck.png')] h-[500px] rounded-xl">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ourservice;
